68a70a96355000294baaab9f51eaa2ca27a766b1
diasmarcos <diasds.marcos@gmail.com>
2023-04-05 11:29:22 -0300

    documentação e comentarios

---
+++ java-terminal/src/Calculadora.java
@@ -0,0 +1,23 @@
+/**
+ * <h1>Calculadora</h1>
+ * A calculadora realiza operações matemativa entre numeros inteiros
+ * <p>
+ * <b>Note:</b> Leia atentamente a documentação desta classe para desfrutar dos recusrsos oferecidos pelo autor
+ * 
+ * @author Marcos Dias
+ * @version 1.0
+ * @since 01/04/2023
+ * 
+ */
+public class Calculadora {
+    /**
+     * Este metodo e utilizado para somar dois numeros inteiros
+     * @param numeroUm este é o primeiro parametro do metodo
+     * @param numeroDois este é o segundo parametro do metodo
+     * @return int o resultado deste metodo é a soma dos dois numeros inteiros
+     * @throws Exception
+     */
+    public int somar(int numeroUm, int nuemroDois) {
+        return numeroUm + nuemroDois;
+    }
+}
---
+++ java-terminal/src/Comentarios.java
@@ -0,0 +1,24 @@
+public class Comentarios {
+    public static void main(String[] args) {
+        // olá, eu sou um comentarios de uma linha só
+
+        /* Olá.
+         * Eu sou um comentario
+         * que posso ser mais detalhado
+         * quando necessário.
+         * com varias linhas.
+         */
+    }
+    /**
+     * Estas duas estrelinhas acima
+     * é utilizada para identificar que você 
+     * pretende elaborar um comentário
+     * a nivel de DOCUMENTAÇÃO.
+     * Otimo isso, né!! 
+     * 
+     */
+    public void NovoMetodo() {
+        
+    }
+    
+}
